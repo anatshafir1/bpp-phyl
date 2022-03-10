7a86d6768955e26293844645ff2abb63fb671aec
anatshafir1 <anatshafir1@mail.tau.ac.il>
2022-03-10 18:02:57 +0200

    solving segfault bug

--- src/Bpp/Phyl/Likelihood/ChromosomeNumberOptimizer.cpp
+++ src/Bpp/Phyl/Likelihood/ChromosomeNumberOptimizer.cpp
@@ -73,7 +73,7 @@ void ChromosomeNumberOptimizer::initLikelihoods(std::map<uint, std::pair<int, st
             lik = setRandomHeterogeneousModel(tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels, parsimonyBound * (double)n, fixedParams, sharedParams);
     
         }
-        ifNanTryToResampleLikObject(lik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels, parsimonyBound, numOfPoints, fixedParams, sharedParams);
+        ifNanTryToResampleLikObject(&lik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels, parsimonyBound, numOfPoints, fixedParams, sharedParams);
         // int numOfTrials
         
         // while((std::isnan(lik->getValue())) && (ChromEvolOptions::maxNumOfTrials_)){
@@ -1253,7 +1253,7 @@ SingleProcessPhyloLikelihood* ChromosomeNumberOptimizer::getBackwardLikObject(st
     }else{
         newLik = setRandomHeterogeneousModel(tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels, parsimonyBound * (double)iteration, fixedParams, updatedSharedParams);
     }
-    ifNanTryToResampleLikObject(newLik, tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels, parsimonyBound, iteration, fixedParams, updatedSharedParams);
+    ifNanTryToResampleLikObject(&newLik, tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels, parsimonyBound, iteration, fixedParams, updatedSharedParams);
     return newLik;
 
 }
@@ -1267,7 +1267,7 @@ SingleProcessPhyloLikelihood* ChromosomeNumberOptimizer::getSingleNewLikObject(s
     }else{
         newLik = setRandomHeterogeneousModel(tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound * (double)iteration, fixedParams, updatedSharedParams);
     }
-    ifNanTryToResampleLikObject(newLik, tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, iteration, fixedParams, updatedSharedParams);
+    ifNanTryToResampleLikObject(&newLik, tree_, vsc_, alphabet_, *baseNumberBounds, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, iteration, fixedParams, updatedSharedParams);
     return newLik;
     
 
@@ -1319,7 +1319,7 @@ void ChromosomeNumberOptimizer::getNewLikObjectForParallelRuns(std::vector<Singl
             // setRandomHeterogeneousModel(tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, ChromEvolOptions::numOfModels_, parsimonyBound * (double)n, fixedParams_, &(ChromEvolOptions::sharedParameters_));
             newLik = setRandomHeterogeneousModel(tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound * (double)i, fixedParams, updatedSharedParams);
         }
-        ifNanTryToResampleLikObject(newLik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, numOfPoints, fixedParams, updatedSharedParams);
+        ifNanTryToResampleLikObject(&newLik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, numOfPoints, fixedParams, updatedSharedParams);
         perCandidateLikVec.push_back(newLik);
     }
     
@@ -1370,7 +1370,7 @@ void ChromosomeNumberOptimizer::getNewLikObject(SingleProcessPhyloLikelihood* cu
             // setRandomHeterogeneousModel(tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, ChromEvolOptions::numOfModels_, parsimonyBound * (double)n, fixedParams_, &(ChromEvolOptions::sharedParameters_));
             newLik = setRandomHeterogeneousModel(tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound * (double)i, fixedParams, updatedSharedParams);
         }
-        ifNanTryToResampleLikObject(newLik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, numOfPoints, fixedParams, updatedSharedParams);
+        ifNanTryToResampleLikObject(&newLik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels+1, parsimonyBound, numOfPoints, fixedParams, updatedSharedParams);
         vectorOfLikelohoods_.push_back(newLik);
     }
     
@@ -1416,7 +1416,7 @@ std::map<uint, pair<int, std::map<int, std::vector<double>>>> ChromosomeNumberOp
 }
 /***********************************************************************************************/
 //ifNanTryToResampleLikObject(lik, tree_, vsc_, alphabet_, baseNumberUpperBound_, mapModelNodesIds, modelParams, numOfModels, parsimonyBound, numOfPoints, fixedParams, sharedParams);
-void ChromosomeNumberOptimizer::ifNanTryToResampleLikObject(SingleProcessPhyloLikelihood* lik, const PhyloTree* tree, const VectorSiteContainer* vsc, const ChromosomeAlphabet* alphabet,
+void ChromosomeNumberOptimizer::ifNanTryToResampleLikObject(SingleProcessPhyloLikelihood** lik, const PhyloTree* tree, const VectorSiteContainer* vsc, const ChromosomeAlphabet* alphabet,
     std::map<uint, uint> baseNumberUpperBound, std::map<uint, vector<uint>> &mapModelNodesIds, 
     std::map<uint, pair<int, std::map<int, std::vector<double>>>> &modelParams, 
     uint numOfModels, double parsimonyBound, int numOfPoints,
@@ -1424,24 +1424,24 @@ void ChromosomeNumberOptimizer::ifNanTryToResampleLikObject(SingleProcessPhyloLi
     std::map<int, std::vector<std::pair<uint, int>>>* sharedParams)
 {
     int numOfTrials = 0;
-    while((std::isnan(lik->getValue())) && (numOfTrials < ChromEvolOptions::maxNumOfTrials_)){
+    while((std::isnan((*lik)->getValue())) && (numOfTrials < ChromEvolOptions::maxNumOfTrials_)){
         std::cerr << "WARNING!!! The value of the likelihood object is nan"<< std::endl;
-        auto likToDel = lik; 
+        auto likToDel = *lik; 
         deleteLikObject(likToDel);
-        int factor;
+        int factor = 0;
         if (numOfPoints == 0){
             factor ++;
         }else{
             factor = numOfPoints;
         }
-        auto multiplier = RandomTools::giveIntRandomNumberBetweenZeroAndEntry<size_t>(numOfPoints);
+        auto multiplier = RandomTools::giveIntRandomNumberBetweenZeroAndEntry<size_t>(factor);
         if (multiplier == 0){
             multiplier ++;
         }
-        lik = setRandomHeterogeneousModel(tree, vsc, alphabet, baseNumberUpperBound, mapModelNodesIds, modelParams, numOfModels, parsimonyBound * (double)multiplier, fixedParams, sharedParams);
+        *lik = setRandomHeterogeneousModel(tree, vsc, alphabet, baseNumberUpperBound, mapModelNodesIds, modelParams, numOfModels, parsimonyBound * (double)multiplier, fixedParams, sharedParams);
         numOfTrials ++;
     }
-    if (std::isnan(lik->getValue())){
+    if (std::isnan((*lik)->getValue())){
         throw Exception("ERROR!!! The likelihood point is nan!!!!\n");
     }
 }
