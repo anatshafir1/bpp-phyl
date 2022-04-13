2bb1622ec0ececdc8b15f84dd5bf444faf08bab5
anatshafir1 <anatshafir1@mail.tau.ac.il>
2022-04-13 13:22:27 +0300

    reverting

--- src/Bpp/Phyl/App/ChromEvolOptions.cpp
+++ src/Bpp/Phyl/App/ChromEvolOptions.cpp
@@ -89,7 +89,7 @@ void ChromEvolOptions::initDefaultParameters(){
     numOfDataToSimulate_ = 1;
     maxBaseNumTransition_[1] = 18;
     treeLength_ = 0;
-    maxNumOfTrials_ = 15;
+    maxNumOfTrials_ = 10;
     minCladeSize_ = 2;
     maxNumOfModels_ = 1;
     heterogeneousModel_ = false; // the default is homogeneous model
